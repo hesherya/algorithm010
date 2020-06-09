432d8439d100be8b6ff6abc990933ebae6b61c08
Hesher Ya <haizhou.ya@mail.com>
2020-06-09 14:25:50 +0800

    feat(week01): Add move zeroes
    Refactor(week01):Reformat

---
+++ Week01/283_move_zeroes.java
@@ -0,0 +1,43 @@
+// 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+// 示例: 
+//
+// 输入: [0,1,0,3,12]
+// 输出: [1,3,12,0,0]
+//
+// 说明: 
+//
+// 
+// 必须在原数组上操作，不能拷贝额外的数组。 
+// 尽量减少操作次数。 
+// 
+// Related Topics 数组 双指针
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void moveZeroes(int[] nums) {
+//        if (nums == null) return;
+//        int lastNonzero = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] != 0) {
+//                int tmp = nums[i];
+//                nums[i] = nums[lastNonzero];
+//                nums[lastNonzero++] = tmp;
+//            }
+//        }
+
+        if (nums == null) {
+            return;
+        }
+        int insertIndex = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertIndex++] = num;
+            }
+        }
+        while (insertIndex < nums.length) {
+            nums[insertIndex++] = 0;
+        }
+    }
+}
