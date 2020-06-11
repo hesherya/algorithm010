3db8629b0921641b322647c45c82f77a8a92d928
Hesher Ya <haizhou.ya@mail.com>
2020-06-11 16:42:20 +0800

    feat(week01): Add Others directory and some excercises

---
+++ Week01/Others/11_max_area.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=java
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxArea;
+    }
+}
+// @lc code=end
+
+
