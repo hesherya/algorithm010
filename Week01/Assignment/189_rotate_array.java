5f10a05973164ec300fe5b0d461e81b9f0f23636
Hesher Ya <haizhou.ya@mail.com>
2020-06-15 00:32:38 +0800

    feat(week01): Add others solution for No. 189

--- Week01/Assignment/189_rotate_array.java
+++ Week01/Assignment/189_rotate_array.java
@@ -1,6 +1,6 @@
 // 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
 //
-// 示例 1: 
+// 示例 1:
 //
 // 输入: [1,2,3,4,5,6,7] 和 k = 3
 // 输出: [5,6,7,1,2,3,4]
@@ -8,9 +8,9 @@
 // 向右旋转 1 步: [7,1,2,3,4,5,6]
 // 向右旋转 2 步: [6,7,1,2,3,4,5]
 // 向右旋转 3 步: [5,6,7,1,2,3,4]
-// 
 //
-// 示例 2: 
+//
+// 示例 2:
 //
 // 输入: [-1,-100,3,99] 和 k = 2
 // 输出: [3,99,-1,-100]
@@ -18,30 +18,55 @@
 // 向右旋转 1 步: [99,-1,-100,3]
 // 向右旋转 2 步: [3,99,-1,-100]
 //
-// 说明: 
+// 说明:
 //
-// 
-// 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。 
-// 要求使用空间复杂度为 O(1) 的 原地 算法。 
-// 
+//
+// 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+// 要求使用空间复杂度为 O(1) 的 原地 算法。
+//
 // Related Topics 数组
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void rotate(int[] nums, int k) {
-        // 暴力法
+        // 1. 暴力法
         if (nums == null || k <= 0) return;
-        while (k > 0) {
-            int prev = nums[nums.length - 1];
-            for (int i = 0; i < nums.length; ++i) {
-                int temp = nums[i];
-                nums[i] = prev;
-                prev = temp;
-            }
-            --k;
+//        while (k > 0) {
+//            int prev = nums[nums.length - 1];
+//            for (int i = 0; i < nums.length; ++i) {
+//                int temp = nums[i];
+//                nums[i] = prev;
+//                prev = temp;
+//            }
+//            --k;
+//        }
+
+        // 2. 额外数组
+//        int[] a = new int[nums.length];
+//        for (int i = 0; i < nums.length; ++i) {
+//            a[(i + k) % nums.length] = nums[i];
+//        }
+//        for (int i = 0; i < nums.length; ++i) {
+//            nums[i] = a[i];
+//        }
+
+        // 3. 三次反转
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1) ;
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        int temp;
+        while (start < end) {
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
-
